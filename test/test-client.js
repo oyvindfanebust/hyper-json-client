d01b7de8d577dbf0e22ec9c18eb8e6dde8d2c8fd
Oyvind Fanebust <oyvind.fanebust@gmail.com>
2013-09-05 21:40:57 +0200

    Started implementing client

---
+++ test/test-client.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+var client = require('../src/client');
+
+describe('calling a root url', function () {
+	it('should return resource', function () {
+		// var result;
+		// client('http://localhost:1234/api')
+		// 	.success(function(resource) {
+		// 		result = resource;
+		// 	});
+
+		// assert.equals();
+	});
+});
